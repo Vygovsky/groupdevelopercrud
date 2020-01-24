5de66d4b85d39b25d7ca0a0e5b9d59c401388689
Roman_v <vigmol@ukr.net>
2020-01-24 17:02:38 +0200

    add branch

--- src/App.js
+++ src/App.js
@@ -1,65 +1,14 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Service from "./components/Service";
+
+import GroupList from "./components/GroupList";
 
 class App extends Component {
-    Service = new Service();
-    state = {
-        groups: [],
-        isLoading: false
-    };
-
-    /*    async componentDidMount() {
-            const response = await fetch('api/groups');
-            const body = await response.json();
-            this.setState({
-                isLoading: false,
-                groups: body
-            });
-            console.log(this.state.body)
-        };*/
-
-    componentDidMount() {
-        this.Service.getAllGroups()
-            .then(this.getAllGroup)
-            .catch(this.onError);
-    };
-
-    getAllGroup = groups => {
-        this.setState({
-            groups,
-            isLoading: false
-        });
-        console.log(this.state.groups);
-    };
-
-    onError = () => {
-        this.setState({
-            isLoading: true
-        })
-    };
-
 
     render() {
-        const {isLoading, groups} = this.state;
-
-        if (isLoading) {
-            return <h1>...Loading</h1>
-        }
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <div className="App-intro">
-                        <h2>Group List</h2>
-                        {groups.map(group =>
-                            <div key={group.id}>
-                                {group.name}
-                            </div>
-                        )}
-                    </div>
-                </header>
+                <GroupList/>
             </div>
 
         );
--- src/components/GroupList.jsx
+++ src/components/GroupList.jsx
@@ -1,6 +1,69 @@
 import React, {Component} from 'react';
+import Service from "./Service";
 
 class GroupList extends Component {
+    Service = new Service();
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            groups: []
+        }
+    }
 
+    /*    async componentDidMount() {
+            const response = await fetch('api/groups');
+            const body = await response.json();
+            this.setState({
+                isLoading: false,
+                groups: body
+            });
+            console.log(this.state.body)
+        };*/
+
+    componentDidMount() {
+        this.Service.getAllGroups()
+            .then(this.getAllGroup)
+            .catch(this.onError);
+    };
+
+    getAllGroup = groups => {
+        this.setState({
+            groups,
+            isLoading: false
+        });
+        console.log(this.state.groups);
+    };
+
+    onError = () => {
+        this.setState({
+            isLoading: true
+        })
+    };
+
+    render() {
+        const {isLoading, groups} = this.state;
+
+        if (isLoading) {
+            return <h1>...Loading</h1>
+        }
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <div className="App-intro">
+                        <h2>Group List</h2>
+                        {groups.map(group =>
+                            <div key={group.id}>
+                                {group.name}
+                            </div>
+                        )}
+                    </div>
+                </header>
+            </div>
+
+        );
+    }
 }
+
+export default GroupList;
